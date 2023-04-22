c0e499a43aa5ad84d37fdcfc4e1af7b926d8c5f4
ChrisOboe <chris@oboe.email>
2023-04-22 15:17:57 +0200

    update stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "SPIFFS.h"
 #include "esp_system.h"
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 #include <AudioFileSourceSPIFFS.h>
 #include <AudioGeneratorWAV.h>
 #include <AudioOutputI2S.h>
 #include <SD.h>
+#include <TelnetStream.h>
 #include <WiFi.h>
 #include <Wire.h>
 #include <tas5805m.hpp>
@@ -41,14 +43,51 @@ void wifiLoop() {
   WiFi.scanNetworks();
   WiFi.begin("gob", "pfefferpapageiknotenhaken");
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  };
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    TelnetStream.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
 };
+
+void otaInit() {
+
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
+        // using SPIFFS.end()
+        TelnetStream.println("Start updating " + type);
+      })
+      .onEnd([]() { TelnetStream.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        TelnetStream.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        TelnetStream.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          TelnetStream.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          TelnetStream.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          TelnetStream.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          TelnetStream.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          TelnetStream.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
+}
 
 void wifiInit() {
   WiFi.mode(WIFI_STA);
-  WiFi.setHostname(esp32 - doorbell);
+  WiFi.setHostname("doorbell");
   WiFi.disconnect();
   delay(100);
   wifiLoop();
@@ -66,9 +105,9 @@ void onLoudnessChange(HANumeric number, HANumber *sender) {
   out->SetGain(float(number.toInt8()) / 100.0);
 };
 
-void buttonInit() { pinMode(2, INPUT_PULLUP); }
+void buttonInit() { pinMode(4, INPUT_PULLUP); }
 void buttonLoop() {
-  int state = digitalRead(2);
+  int state = digitalRead(4);
   bool lastButtonState = buttonPressed;
   if (state == 0) {
     buttonPressed = true;
@@ -79,7 +118,7 @@ void buttonLoop() {
   }
 
   if (lastButtonState == false && buttonPressed == true) {
-    Serial.println("Button pressed!");
+    TelnetStream.println("Button pressed!");
     audioPlay();
   }
 };
@@ -119,10 +158,10 @@ void mqttInit() {
 void audioInit() {
   Tas5805m.init();
   out = new AudioOutputI2S();
-  Serial.printf("Setting I2S pins: clk = %d, ws = %d, data = %d\n", PIN_I2S_SCK,
-                PIN_I2S_FS, PIN_I2S_SD);
+  TelnetStream.printf("Setting I2S pins: clk = %d, ws = %d, data = %d\n",
+                      PIN_I2S_SCK, PIN_I2S_FS, PIN_I2S_SD);
   if (!out->SetPinout(PIN_I2S_SCK, PIN_I2S_FS, PIN_I2S_SD)) {
-    Serial.println("Failed to set pinout");
+    TelnetStream.println("Failed to set pinout");
   }
 
   Tas5805m.begin();
@@ -131,22 +170,26 @@ void audioInit() {
 }
 
 void setup() {
-  Serial.begin(SERIAL_BAUD);
-  Serial.println(F("Starting up...\n"));
+  wifiInit();
+  TelnetStream.begin();
+
+  // Serial.begin(SERIAL_BAUD);
+  TelnetStream.println(F("Starting up...\n"));
 
   if (!SPIFFS.begin()) {
-    Serial.println(F("An Error has occurred while mounting SPIFFS"));
+    TelnetStream.println(F("An Error has occurred while mounting SPIFFS"));
     while (true) {
     };
   } else
-    Serial.println(F("SPIFFS mounted"));
+    TelnetStream.println(F("SPIFFS mounted"));
 
   Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL);
 
   audioInit();
   buttonInit();
-  wifiInit();
+
   mqttInit();
+  otaInit();
 }
 
 void loop() {
@@ -154,4 +197,5 @@ void loop() {
   mqtt.loop();
   audioLoop();
   buttonLoop();
+  ArduinoOTA.handle();
 }
